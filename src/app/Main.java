ec175d088dce378b657a630372e7ead0fad6f596
DmitriyMagnis <chernobrivetsdmitriy2@gmail.com>
2024-11-13 11:10:08 +0100

    ADD/basic functionality and usage

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,5 +3,14 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("welcome to WordCorrector app");
+
+        String output = Corrector.handleData(
+                new DataProvider().getData()
+        );
+
+        getOutput(output);
+    }
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
